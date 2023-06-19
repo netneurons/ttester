5fc481c2a9efaf2bc56c6fd8327095f96e105c40
Glitch (glitch-hello-website) <none>
2023-06-19 18:47:05 +0000

    🗼🏛 Checkpoint ./t.html:59089184/338 ./script.js:59089184/700

--- script.js
+++ script.js
@@ -8,6 +8,18 @@ function send(){
   document.querySelector('input').value = '';
   var uall = [];
   var message = {
-    
-  }
+    sender: 'user',
+    msg: msg
+  };
+  uall.push(message);
+  receive(uall);
+}
+function receive(msgs){
+  var ms = msgs;
+  var str = JSON.parse(ms);
+var msg = str[str.length - 1].msg;
+  var d= new Date();
+  var date = d.toTimeString();
+  var inner = '<div class="flex w-full mt-2 space-x-3 max-w-xs ml-auto justify-end"><div><div class="bg-blue-600 text-white p-3 rounded-l-lg rounded-br-lg"><p class="text-sm">'+ msg +'</p></div><span class="text-xs text-gray-500 leading-none">'+date+'</span></div><div class="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300"></div></div>';
+  parent.innerHTML+=inner;
 }
