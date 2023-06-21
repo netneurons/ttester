7a04d728c7623cd780b27a118535daf64ade6020
Glitch (glitch-hello-website) <none>
2023-06-21 15:52:04 +0000

    💺✋ Checkpoint ./script.js:59089184/588 ./t.html:59089184/2392

--- script.js
+++ script.js
@@ -6,8 +6,10 @@ function send(){
   var inner = '<div class="flex w-full mt-2 space-x-3 max-w-xs ml-auto justify-end"><div><div class="bg-blue-600 text-white p-3 rounded-l-lg rounded-br-lg"><p class="text-sm">'+ mssg +'</p></div><span class="text-xs text-gray-500 leading-none">'+date+'</span></div><div class="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300"></div></div>';
   if(parent.textContent === 'Nothing Here, write new message.'){
   parent.innerHTML=inner
+  document.querySelector('.divw').scrollTop = document.querySelector('.divw').scrollHeight;
   }else{
   parent.innerHTML+=inner;
+  document.querySelector('.divw').scrollTop = document.querySelector('.divw').scrollHeight;
   }
   document.querySelector('input').value = '';
   res(mssg);
@@ -54,6 +56,7 @@ async function res(m){
         var parent = document.querySelectorAll('div')[1];
         var inner = '<div class="flex w-full mt-2 space-x-3 max-w-xs"><div class="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300"></div><div><div class="bg-gray-300 p-3 rounded-r-lg rounded-bl-lg"><p class="text-sm">'+msg+'</p></div><span class="text-xs text-gray-500 leading-none">'+date+'</span></div></div>';
         parent.innerHTML+=inner;
+        document.querySelector('.divw').scrollTop = document.querySelector('.divw').scrollHeight;
         document.querySelector('input').value = '';
         hljs.highlightAll();
 }).catch(error => console.error(error));
@@ -89,6 +92,7 @@ async function ress(m){
         var parent = document.querySelectorAll('div')[1];
         var inner = '<div class="flex w-full mt-2 space-x-3 max-w-xs"><div class="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300"></div><div><div class="bg-gray-300 p-3 rounded-r-lg rounded-bl-lg"><p class="text-sm">'+msg+'</p></div><span class="text-xs text-gray-500 leading-none">'+date+'</span></div></div>';
         parent.innerHTML+=inner;
+        document.querySelector('.divw').scrollTop = document.querySelector('.divw').scrollHeight;
         document.querySelector('input').value = '';
         hljs.highlightAll();
 }).catch(error => console.error(error));
