5ad8fd2f8c2525a98b88b452002beabf3ab92a77
Glitch (torgpt) <none>
2023-07-06 21:22:52 +0000

    👧🌱 Checkpoint ./public/script.js:59089184/3904

--- public/script.js
+++ public/script.js
@@ -185,5 +185,79 @@ function migrate(){
   var btn = document.querySelector('button');
   btn.setAttribute('onclick','send2()');
   s1 = send2
+};
+var m = [];
+var socket = new WebSocket('wss://wss-1xw1.onrender.com', [], { pingInterval: 5000 });
+
+          socket.addEventListener('open', event => {
+            console.log('WebSocket connection opened');
+          });
+function senc(){
+  var t = document.querySelector('input').value;
+  var s = ver(t);
+  message(t,s);
 }
-function message(text, san)
+function message(text, san){
+  var parent = document.querySelector('.divw');
+  var d= new Date();
+  var date = d.toTimeString();
+  var inner = '<div class="flex w-full mt-2 space-x-3 max-w-xs ml-auto justify-end"><div><div class="bg-blue-600 text-white p-3 rounded-l-lg rounded-br-lg"><p class="text-sm divw">'+ san +'</p></div><span class="text-xs text-gray-500 leading-none">'+date+'</span></div><div class="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300 container flex justify-center items-center"><i class="fa fa-user" class="mx-auto"></i></div></div>';
+  if(parent.textContent === 'Nothing Here, write new message.'){
+  parent.innerHTML=inner
+  document.querySelector('.divw').scrollTop = document.querySelector('.divw').scrollHeight;
+  }else{
+  parent.innerHTML+=inner;
+  document.querySelector('.divw').scrollTop = document.querySelector('.divw').scrollHeight;
+  }
+  document.querySelector('input').value = '';
+  var json = {
+    sender:"user",
+    message:text
+  };
+  m.push(json);
+  socket.send(JSON.stringify(m));
+  }
+socket.addEventListener('message', event => {
+            console.log('Received message:', event.data);
+                var b = {
+                          sender: "bot",
+                          message: event.data
+                    };
+                    m.push(b);
+           receive(event.data);
+});
+socket.addEventListener('close', event => {
+            console.log('WebSocket connection closed');
+          });
+
+          socket.addEventListener('error', event => {
+            console.error('WebSocket error:', event);
+          });
+function receive(text){
+  const t = window.markdownit({
+        html: true,                       
+        linkify: true,                    
+        typographer: true,                
+        highlight: (str, lang) => {       
+          if (lang && hljs.getLanguage(lang)) {
+            try {
+              var option = {language:lang, ignoreIllegals: true};
+              return '<pre class="hljs"><code>' +
+                     hljs.highlight(str, option).value +
+                     '</code></pre>';
+            } catch (__) {}
+          }
+    
+          return '<pre class="hljs"><code>' + t.utils.escapeHtml(str) + '</code></pre>';
+        },
+      })
+        const msg = t.render(text);
+        var d = new Date();
+        var date = d.toTimeString();
+        var parent = document.querySelector('.divw');
+        var inner = '<div class="flex w-full mt-2 space-x-3 max-w-xs"><div class="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300 container flex justify-center items-center"><i class="fa fa-server mx-auto"></i></div><div><div class="bg-gray-300 p-3 rounded-r-lg rounded-bl-lg"><p class="text-sm divw">'+msg+'</p></div><span class="text-xs text-gray-500 leading-none">'+date+'</span></div></div>';
+        parent.innerHTML+=inner;
+        document.querySelector('.divw').scrollTop = document.querySelector('.divw').scrollHeight;
+        document.querySelector('input').value = '';
+        hljs.highlightAll();
+}
